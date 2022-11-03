6ff70226c5e416001e15991704c6eb4dab24085d
MalcolmSmalls <smallsbeats@gmail.com>
2022-11-02 17:59:52 -0700

    Add some state logic, add button and input field

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <div></div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state ={
+      item: "",
+    }
+
+    this.addTodo = this.addTodo.bind(this)
+  }
+
+  addTodo(){
+    const { item } = this.state 
+    this.setState ({ 
+      item: this.state.item
+    })
+  }
+
+  render(){
+    return (
+      <div>
+        <input></input>
+        <button onClick={this.addToo}>Submit</button>
+      </div>
+    )
+  }
 }
 
 export default App;
